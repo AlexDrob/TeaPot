10d1e32e94bd9e88e667280e93e88ca718e8f427
AlexDrob <eess9@rambler.ru>
2017-01-21 17:55:26 +0200

    Начата реализация меню

--- app/src/main/java/com/bignerdranch/android/teapot/TeapotActivity.java
+++ app/src/main/java/com/bignerdranch/android/teapot/TeapotActivity.java
@@ -1,21 +1,72 @@
 package com.bignerdranch.android.teapot;
 
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class TeapotActivity extends FragmentActivity {
+public class TeapotActivity extends ActionBarActivity {
 
     private static final String TAG = "TeapotActivity";
+
+    private DrawerLayout myDrawerLayout;
+    private ListView myDrawerList;
+    private ActionBarDrawerToggle myDrawerToggle;
+
+    // navigation drawer title
+    private CharSequence myDrawerTitle;
+    // used to store app title
+    private CharSequence myTitle;
+
+    private String[] viewsNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teapot);
+
+        myTitle =  getTitle();
+        myDrawerTitle = getResources().getString(R.string.menu);
+
+        // load slide menu items
+        viewsNames = getResources().getStringArray(R.array.views_array);
+        myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        myDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        myDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, viewsNames));
+
+        // enabling action bar app icon and behaving it as toggle button
+        //android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
+        /*myDrawerToggle = new ActionBarDrawerToggle(this, myDrawerLayout,
+                R.string.open_menu,
+                R.string.close_menu
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(myTitle);
+                // calling onPrepareOptionsMenu() to show action bar icons
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(myDrawerTitle);
+                // calling onPrepareOptionsMenu() to hide action bar icons
+                invalidateOptionsMenu();
+            }
+        };
+        myDrawerLayout.setDrawerListener(myDrawerToggle); */
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.teapot_main_frame_container);
